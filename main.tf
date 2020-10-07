0bad7c9606e9dfba5f63acf59b71da5b8872c642
Aditya Vashistha <adityavashita@adityas-mbp.myfiosgateway.com>
2020-10-07 16:06:59 -0400

    Added resources for setting up a Scalable reverse proxy infrastructure

---
+++ main.tf
@@ -0,0 +1,124 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  version = "~> 3.39.0"
+}
+
+/**
+ * Task 1: Add a Global HTTP Load Balancer ("load_balancer")
+ * - source: "GoogleCloudPlatform/lb-http/google"
+ * - version: "~> 3.1.0"
+ * - project: module.project_iam_bindings.projects[0]
+ * - name: "lab05-http-load-balancer"
+ * - firewall_networks: module.network.network_self_link
+ * - target_tags: var.target_tags
+ * - backends: (Set rest of inputs to null)
+ *   - default
+ *     - port_name: "http"
+ *     - protocol: "HTTP"
+ *     - port: 80
+ *     - description: null
+ *     - timeout_sec: null
+ *     - connection_draining_timeout_sec: null
+ *     - enable_cdn: null
+ *     - health_check:
+ *       - request_path: "/"
+ *       - port: 80
+ *       - check_interval_sec:  null
+ *       - timeout_sec:         null
+ *       - healthy_threshold:   null
+ *       - unhealthy_threshold: null
+ *       - host:                null
+ *   - groups:
+ *     - group: module.managed_instance_group.instance_group
+ *     - balancing_mode: null
+ *     - capacity_scaler: null
+ *     - description: null
+ *     - max_connections: null
+ *     - max_connections_per_instance: null
+ *     - max_connections_per_endpoint: null
+ *     - max_rate: null
+ *     - max_rate_per_instance: null
+ *     - max_rate_per_endpoint: null
+ *     - max_utilization: null
+ *
+ * Reference - https://github.com/terraform-google-modules/terraform-google-lb-http
+ *
+ */
+
+resource "google_compute_forwarding_rule" "default" {  
+  name         = var.fr_name
+  region       = var.region
+  network      = module.network.network_name
+  subnetwork   = module.network.subnets_self_links[0]
+  project      = module.project_iam_bindings.projects[0]
+  backend_service       = google_compute_region_backend_service.default.id
+  load_balancing_scheme = "INTERNAL"
+  ports = var.ports
+  ip_address   = google_compute_address.internal_with_subnet_and_address.address
+}
+
+resource "google_compute_health_check" "http_hc" {
+  name                = "hc-http-80"
+  check_interval_sec  = 4
+  timeout_sec         = 4
+  healthy_threshold   = 2
+  unhealthy_threshold = 4
+  
+  http_health_check {
+    port = 80
+  }
+
+  description = "This HC returns OK depending on the method"
+}
+
+resource "google_compute_region_backend_service" "default" {
+
+  backend {
+    group          = module.managed_instance_group.instance_group
+    balancing_mode = "CONNECTION"
+  }
+
+  region      = var.region
+  name        = var.ilb_name
+  protocol    = "TCP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_health_check.http_hc.id]
+}
+
+# module "gce-ilb" {
+#   source            = "GoogleCloudPlatform/lb-internal/google"
+#   version           = "~> 2.0"
+#   name              = "cdle-sf-api-ilb"
+#   project           = module.project_iam_bindings.projects[0]
+#   //firewall_networks = [module.network.network_self_link]
+#   soure_tags        = 
+#   target_tags       = var.target_tags
+#   region            = var.region
+#   port              = 80
+#   health_check      = google_compute_health_check.http_hc
+
+#   backends = [
+#   {
+#     group       = module.managed_instance_group.instance_group
+#     description = "Backend module"
+#   },
+#   }
+# }
---
+++ network.tf
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+resource "google_compute_address" "external_ip_address" {
+  name = var.ext_ip_name
+  address_type = "EXTERNAL"
+}
+
+module "network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 2.5.0"
+  project_id   = module.project_iam_bindings.projects[0]
+  network_name = var.network_name
+  routing_mode = "GLOBAL"
+  subnets = [
+    {
+      subnet_name   = var.subnet_name
+      subnet_ip     = "10.1.2.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
+
+resource "google_compute_address" "internal_with_subnet_and_address" {
+  name         = "cdle-sf-api-internal-address"
+  subnetwork   = module.network.subnets_self_links[0]
+  address_type = "INTERNAL"
+  address      = var.internal_ip
+  region       = var.region
+}
+
+module "cloud_nat" {
+  source        = "terraform-google-modules/cloud-nat/google"
+  version       = "~> 1.3.0"
+  project_id    = module.project_iam_bindings.projects[0]
+  region        = var.region
+  create_router = true
+  router        = var.router_name
+  network       = module.network.network_name
+  //nat_ip_allocate_option ="MANUAL_ONLY"
+  nat_ips       = [google_compute_address.external_ip_address.self_link]
+}
+
+resource "google_vpc_access_connector" "connector" {
+  name          = var.vpc_connector
+  region        = var.region
+  ip_cidr_range = "10.8.0.0/28"
+  network       = module.network.network_name
+}
+
